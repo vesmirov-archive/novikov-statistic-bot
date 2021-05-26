064088730bf8f48febfe9c3d608ae9c108449a08
Evan Vilagov <evan.vilagov@gmail.com>
2021-05-26 03:14:42 +0300

    updated crontab job

--- scheduler.py
+++ scheduler.py
@@ -7,7 +7,7 @@ env = dotenv_values('.env')
 USER=env.get('CHRON_USER')
 
 cron = CronTab(user=USER)
-job = cron.new(command='python notifier.py')
+job = cron.new(command='.venv/bin/python notifier.py')
 job.minute.every(1)
 
 cron.write()
