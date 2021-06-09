028ba4184ce5c6ae74f6b663297390a6095803d9
Evan Vilagov <evan.vilagov@gmail.com>
2021-06-09 15:49:34 +0300

    Updated scheduler.py where PATH scpecified before each command

--- scheduler.py
+++ scheduler.py
@@ -7,13 +7,12 @@ USER = getpass.getuser()
 
 
 cron = CronTab(user=USER)
-cron.env['HOME'] = os.getcwd()
-cron.env['PATH'] = os.getcwd()
+PATH = os.getcwd()
 
-notify = cron.new(command='.venv/bin/python notifier.py')
+notify = cron.new(command=f'{PATH}/.venv/bin/python notifier.py')
 notify.setall('0 22 * * *')
 
-update = cron.new(command='.venv/bin/python updater.py')
+update = cron.new(command=f'{PATH}/.venv/bin/python updater.py')
 update.setall('0 20 * * *')
 
 cron.write()
