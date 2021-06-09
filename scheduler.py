02cff17e98db510db705191e683a13943826c81c
Evan Vilagov <evan.vilagov@gmail.com>
2021-06-09 19:12:08 +0300

    Fixed some typos

--- scheduler.py
+++ scheduler.py
@@ -7,12 +7,14 @@ USER = getpass.getuser()
 
 
 cron = CronTab(user=USER)
-PATH = os.getcwd()
+cron.env['PATH'] = f'/home/{USER}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'  # noqa
 
-notify = cron.new(command=f'{PATH}/.venv/bin/python notifier.py')
+cwd = os.getcwd()
+
+notify = cron.new(command=f'{cwd}/.venv/bin/python {cwd}/notifier.py')
 notify.setall('0 22 * * *')
 
-update = cron.new(command=f'{PATH}/.venv/bin/python updater.py')
+update = cron.new(command=f'{cwd}/.venv/bin/python {cwd}/updater.py')
 update.setall('0 20 * * *')
 
 cron.write()
